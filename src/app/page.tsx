9a98d75defac9abee829eb91b96d0e5113bab36e
Wilkinson <wilkinsondjm@gmail.com>
2024-12-16 23:17:21 -0300

    inclusao depoimentos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { Banner } from "./components/Banner";
 import { Contact } from "./components/Contact";
 import { Depoimentos } from "./components/Depoimentos";
 import { Footer } from "./components/Footer";
-import { Parcerias } from "./components/Parcerias";
 import { ScrollTop } from "./components/ScrollToTopp";
 import { Whatsapp } from "./components/Whatsapp";
 
